3b14e5e264b68c8a367b7eac8071c0248243e558
z̵q̵ <ziku@me.com>
2021-11-27 00:46:04 +0900

    organized code

--- keyboards/lily58/keymaps/zhiqoo/rules.mk
+++ keyboards/lily58/keymaps/zhiqoo/rules.mk
@@ -12,7 +12,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE= yes            # OLED display
+OLED_ENABLE = yes           # OLED display
 
 AUTO_SHIFT_ENABLE = no
 
