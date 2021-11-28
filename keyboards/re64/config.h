d2a74a9411e2224b5a30b92c3b65b9e17fe66348
z̵q̵ <ziku@me.com>
2021-11-29 01:11:51 +0900

    simplified

--- keyboards/re64/config.h
+++ keyboards/re64/config.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include "config_common.h"
-// #define OLED_FONT_H "./lib/glcdfont.c"
+
 #define OLED_FONT_H "../../drivers/oled/glcdfont.c"
 
-/* USB Device descriptor parameter */
 #define VENDOR_ID    0x3938
 #define PRODUCT_ID   0x3634
 #define DEVICE_VER   0x0002
@@ -12,134 +11,82 @@
 #define PRODUCT      Re64
 #define DESCRIPTION  60% split keyboard
 
-/* key matrix size */
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 7
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- *
- */
 #define MATRIX_ROW_PINS { E6, B4, B3, B2, B5 }
 #define MATRIX_COL_PINS { D4, F4, F5, F6, F7, B1, D7 }
 #define UNUSED_PINS
 
-/* COL2ROW, ROW2COL */
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION COL2ROW // = COL = Anode (+), ROW = Cathode (-)
 
 /* Split hand configration */
-#define SPLIT_HAND_MATRIX_GRID B5, B1
-#define SPLIT_HAND_MATRIX_GRID_LOW_IS_RIGHT
+// #define SPLIT_HAND_MATRIX_GRID B5, B1
+// #define SPLIT_HAND_MATRIX_GRID_LOW_IS_RIGHT
+#define MASTER_LEFT
 
-/* Rotary encoder define*/
 #define ENCODERS_PAD_A { C6 }
 #define ENCODERS_PAD_B { B6 }
 #define ENCODERS_PAD_A_RIGHT { B6 }
 #define ENCODERS_PAD_B_RIGHT { C6 }
-//#define ENCODER_RESOLUTION 2
+#define ENCODER_RESOLUTION 2
 
-/*
- * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- */
-#define SOFT_SERIAL_PIN D2  // or D1, D2, D3, E6
 
-//#define BACKLIGHT_PIN B7
-//#define BACKLIGHT_LEVELS 3
-//#define BACKLIGHT_BREATHING
+#define SOFT_SERIAL_PIN D2
+#define SERIAL_USE_MULTI_TRANSACTION
 
-#define RGB_DI_PIN D3
-#ifdef RGB_DI_PIN
-    #define RGBLED_NUM 80
-    #define RGBLED_SPLIT { 40, 40 }
-//#    define RGBLIGHT_HUE_STEP 8
-//#    define RGBLIGHT_SAT_STEP 8
-//#    define RGBLIGHT_VAL_STEP 8
-    #define RGBLIGHT_LIMIT_VAL 112 /* The maximum brightness level */
-//#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*== all animations enable ==*/
-//#    define RGBLIGHT_ANIMATIONS
-/*== or choose animations ==*/
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//#    define RGBLIGHT_EFFECT_SNAKE
-//#    define RGBLIGHT_EFFECT_KNIGHT
-//#    define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//#    define RGBLIGHT_EFFECT_RGB_TEST
-//#    define RGBLIGHT_EFFECT_ALTERNATING
-//#     #define RGBLIGHT_EFFECT_TWINKLE
-/*== customize breathing effect ==*/
-/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-//#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
-/*==== use exp() and sin() ====*/
-//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-#endif
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
-/* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
+#define RGB_DI_PIN D3
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 76
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
+#define RGBLED_SPLIT { 36, 40 }
+#define RGBLIGHT_MAX_LAYERS 4
 
-/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
- * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
- */
-//#define GRAVE_ESC_CTRL_OVERRIDE
+#define LED_LAYOUT( \
+L00, L01, L02, L03, L04, L05, L06,        R00, R01, R02, R03, R04, R05, R06, R07, \
+L10, L11, L12, L13, L14, L15,             R10, R11, R12, R13, R14, R15, R16, R17, \
+L20, L21, L22, L23, L24, L25,             R20, R21, R22, R23, R24, R25, R26, \
+L30, L31, L32, L33, L34, L35,             R30, R31, R32, R33, R34, R35, R36, \
+L40, L41, L42,      L43, L44,             R40, R41,      R42, R43, \
+               L50, L51, L52,             R50, R51, R52, \
+               L60, L61, L62,             R60, R61, R62) \
+{ \
+    L06, L05, L04, L03, L02, L01, L00, \
+    L10, L11, L12, L13, L14, L15, \
+    L25, L24, L23, L22, L21, L20, \
+    L30, L31, L32, L33, L34, L35, \
+    L44, L43, L42,      L41, L40, \
+    L50, L51, L52, \
+    L62, L61, L60, \
+    \
+    R00, R01, R02, R03, R04, R05, R06, R07, \
+    R17, R16, R15, R14, R13, R12, R11, R10, \
+    R20, R21, R22, R23, R24, R25, R26, \
+    R36, R35, R34, R33, R32, R31, R30, \
+    R40, R41,      R42, R43, \
+    R52, R51, R50, \
+    R60, R61, R62 \
+}
 
-/*
- * Force NKRO
- *
- * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
- * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
- * makefile for this to work.)
- *
- * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
- * until the next keyboard reset.
- *
- * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
- * fully operational during normal computer usage.
- *
- * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
- * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
- * bootmagic, NKRO mode will always be enabled until it is toggled again during a
- * power-up.
- *
- */
-//#define FORCE_NKRO
+  #define RGBLIGHT_LED_MAP LED_LAYOUT( \
+     0,  1,  2,  3,  4,  5,  6,        30, 31, 32, 33, 34, 35, 36, 37, \
+     7,  8,  9, 10, 11, 12,            38, 39, 40, 41, 42, 43, 44, 45, \
+    13, 14, 15, 16, 17, 18,            46, 47, 48, 49, 50, 51, 52, \
+    19, 20, 21, 22, 23, 24,            53, 54, 55, 56, 57, 58, 59, \
+    25, 26, 27,     28, 29,            60, 61,     62, 63, \
+                64, 65, 66,            67, 68, 69, \
+                70, 71, 72,            73, 74, 75 \
+)
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
-/* disable these deprecated features by default */
+// #define NO_ACTION_LAYER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-
-/* Bootmagic Lite key configuration */
-//#define BOOTMAGIC_LITE_ROW 0
-//#define BOOTMAGIC_LITE_COLUMN 0
