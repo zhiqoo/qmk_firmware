d1e5e9354972fdb27b61512b734d870f42f77c9b
z̵q̵ <ziku@me.com>
2021-11-30 02:30:16 +0900

    removed unneeded comments

--- keyboards/lily58/keymaps/zhiqoo/rules.mk
+++ keyboards/lily58/keymaps/zhiqoo/rules.mk
@@ -1,7 +1,3 @@
-# Build Options
-#   change to "no" to disable the options, or define them in the Makefile in
-#   the appropriate keymap folder that will get included automatically
-#
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
@@ -16,10 +12,8 @@ OLED_ENABLE = yes           # OLED display
 
 AUTO_SHIFT_ENABLE = no
 
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
-# If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
