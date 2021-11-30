8a89766f6b9d2392eeb946d9673bc0d3e8a09bfb
z̵q̵ <ziku@me.com>
2021-11-30 13:29:03 +0900

    fixed led row

--- keyboards/re64/config.h
+++ keyboards/re64/config.h
@@ -3,7 +3,7 @@
 #include "config_common.h"
 
 #define OLED_FONT_H "./lib/glcdfont_re64.c"
-#define OLED_TIMEOUT 30000
+#define OLED_TIMEOUT 1000
 #define OLED_FADE_OUT
 #define OLED_FADE_OUT_INTERVAL 1
 
@@ -61,13 +61,13 @@ L40, L41, L42,      L43, L44,             R40, R41,      R42, R43, \
                L50, L51, L52,             R50, R51, R52, \
                L60, L61, L62,             R60, R61, R62) \
 { \
-    L06, L05, L04, L03, L02, L01, L00, \
+    L00, L01, L02, L03, L04, L05, L06, \
     L10, L11, L12, L13, L14, L15, \
-    L25, L24, L23, L22, L21, L20, \
+    L20, L21, L22, L23, L24, L25, \
     L30, L31, L32, L33, L34, L35, \
-    L44, L43, L42,      L41, L40, \
+    L40, L41, L42,      L43, L44, \
     L50, L51, L52, \
-    L62, L61, L60, \
+    L60, L61, L62, \
     \
     R00, R01, R02, R03, R04, R05, R06, R07, \
     R17, R16, R15, R14, R13, R12, R11, R10, \
