c6b0790307b775ceb755b6fc1f9a692d49f99b4b
z̵q̵ <ziku@me.com>
2022-04-10 02:41:37 +0900

    via disabled

--- keyboards/lily58/keymaps/zhiqoo/rules.mk
+++ keyboards/lily58/keymaps/zhiqoo/rules.mk
@@ -9,7 +9,7 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE = yes           # OLED display
-VIA_ENABLE = yes
+VIA_ENABLE = no
 
 AUTO_SHIFT_ENABLE = no
 
