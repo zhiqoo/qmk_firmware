fffdc7e08638138535e0adc912e8a42a02613929
z̵q̵ <ziku@me.com>
2022-04-11 21:43:54 +0900

    fixed for VIA

--- keyboards/helix/pico/config.h
+++ keyboards/helix/pico/config.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #define VENDOR_ID       0x3265
-#define PRODUCT_ID      0x0001
+// #define PRODUCT_ID      0x0001
+#define PRODUCT_ID      0x0004 // pretends helix/rev3_4rows to fit VIA
 #define DEVICE_VER      0x0002
 #define MANUFACTURER    Yushakobo
 #define PRODUCT         HelixPico
@@ -10,7 +11,6 @@
 #define TAPPING_TERM 100
 
 #define USE_SERIAL
-
 #define SOFT_SERIAL_PIN D2
 
 #define MASTER_LEFT
@@ -43,32 +43,3 @@
   #define RGBLIGHT_HUE_STEP 10
   #define RGBLIGHT_SAT_STEP 17
 #endif
-
-// #ifndef IOS_DEVICE_ENABLE
-//   #if RGBLED_NUM <= 6
-//     #define RGBLIGHT_LIMIT_VAL 255
-//   #else
-//     #define RGBLIGHT_LIMIT_VAL 130
-//   #endif
-//   #define RGBLIGHT_VAL_STEP 17
-// #else
-//   #if RGBLED_NUM <= 6
-//     #define RGBLIGHT_LIMIT_VAL 90
-//   #else
-//     #define RGBLIGHT_LIMIT_VAL 45
-//   #endif
-//   #define RGBLIGHT_VAL_STEP 4
-// #endif
-
-// #if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
-// // USB_MAX_POWER_CONSUMPTION value for Helix keyboard
-// //  120  RGBoff
-// //  330  RGB 6
-// //  300  RGB 32
-// //  310  OLED & RGB 32
-//   #define USB_MAX_POWER_CONSUMPTION 400
-// #else
-//   // fix iPhone and iPad power adapter issue
-//   // iOS device need lessthan 100
-//   #define USB_MAX_POWER_CONSUMPTION 100
-// #endif
